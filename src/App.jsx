bfa8850a9782131a9f47d106b14e3f05ce4ddfb1
pmsayal <141319042+pmsayal@users.noreply.github.com>
2024-05-08 14:33:06 +0530

    change layout

--- src/App.jsx
+++ src/App.jsx
@@ -16,28 +16,26 @@ import ManagePurchase from "./Purchase-Order/ManagePurchase";
 import AddCustomer from "./AddCustomer";
 import GlobalStyle from "./GlobalStyled";
 
-
 function App() {
   return (
     <>
       <BrowserRouter>
-      <GlobalStyle />
+        <GlobalStyle />
         <Routes>
           <Route path="/" exact element={<Header />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<Signin />} />
-          <Route path="/forgot" element={<ForgotPassword />} />        
-        </Routes>
-        <Routes >
-        <Route path="/" exact element={<ApplayOut />}>
-          <Route path="/" exact element={<Dashboard />} />
-          <Route path="customer" element={<ManageCustomer />} />
-          <Route path="suppliers" element={<ManageSuppliers />} />
-          <Route path="items" element={<ManageItem />} />
-          <Route path="sales" element={<Sales />} />
-          <Route path="purchaseorder" element={<ManagePurchase />} />
-          <Route path="addcustomer" element={<AddCustomer />} />
-        </Route>
+          <Route path="/forgot" element={<ForgotPassword />} />
+
+          <Route path="/" exact element={<ApplayOut />}>
+            <Route path="/dashboard" exact element={<Dashboard />} />
+            <Route path="customer" element={<ManageCustomer />} />
+            <Route path="suppliers" element={<ManageSuppliers />} />
+            <Route path="items" element={<ManageItem />} />
+            <Route path="sales" element={<Sales />} />
+            <Route path="purchaseorder" element={<ManagePurchase />} />
+            <Route path="addcustomer" element={<AddCustomer />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
